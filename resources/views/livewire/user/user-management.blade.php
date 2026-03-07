ac10a375e1dc946efaded8a004b7193d933ea8b2
Ahmed Iswaan Office Lap <ahmed.iswaan@gmail.com>
2026-03-08 00:29:20 +0500

    fix

--- resources/views/livewire/user/user-management.blade.php
+++ resources/views/livewire/user/user-management.blade.php
@@ -151,7 +151,7 @@
                                         </td>
                                         <td data-order="2023-12-20T20:43:00+05:00">{{ \Carbon\Carbon::parse($user->created_at)->format('d M Y, g:i a') }}</td>
                                         <td class="text-end position-relative">
-                                            <a href="#" class="btn btn-light btn-active-light-primary btn-flex btn-center btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
+                                            <a href="javascript:void(0)" class="btn btn-light btn-active-light-primary btn-flex btn-center btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
                                             <i class="ki-duotone ki-down fs-5 ms-1"></i></a>
                                             <!--begin::Menu-->
                                             <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-180px py-4" data-kt-menu="true">
@@ -167,9 +167,9 @@
 
                                                 @can('user-resetPassword')
                                                 <div class="menu-item px-3">
-                                                    <a href="#" class="menu-link px-3" wire:click="openResetPasswordModal({{ $user->id }})">
+                                                    <button type="button" class="menu-link px-3 btn btn-link p-0 text-start w-100" wire:click.prevent="openResetPasswordModal({{ $user->id }})">
                                                         Reset Password
-                                                    </a>
+                                                    </button>
                                                 </div>
                                                 @endcan
 
